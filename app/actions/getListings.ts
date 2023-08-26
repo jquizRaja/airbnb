38059aafb05b583eae73bb1b03c359fa47649022
jquizRaja <138556401+jquizRaja@users.noreply.github.com>
2023-08-26 12:03:06 +0530

    Added safeListings done

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -4,10 +4,14 @@ export default async function getListings() {
   try {
     const listings = await prisma.listing.findMany({
       orderBy: {
-        createdAt: "desc"
-      }
+        createdAt: "desc",
+      },
     });
-    return listings;
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+    return safeListings;
   } catch (error: any) {
     throw new Error(error);
   }
